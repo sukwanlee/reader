63a742c58c33008d09b0dcd659d0769b1c37cf44
Suchan Lee <suchanl@palantir.com>
2014-08-23 20:04:53 -0700

    sukwans glaring endpoint bug fixed

--- routes/index.js
+++ routes/index.js
@@ -14,12 +14,12 @@ router.get('/', function(req, res) {
 });
 
 
-/* GET site posts list */
+/* GET post list */
 router.get('/:slug', function(req, res) {
     var posts;
 
     if (req.params.slug === 'hn') {
-        getPosts('http://api.ihackernews.com/page', 'hacker-news', function(posts) {
+        getPosts('http://api.ihackernews.com/page', req.params.slug, function(posts) {
             res.render('hnList', {
                 posts: posts,
                 title: 'Hacker News',
@@ -38,25 +38,16 @@ router.get('/:slug', function(req, res) {
     }
 });
 
-router.get('/:slug/:postTitle', function(req, res) {
-	console.log(req.params.slug, req.params.postTitle);
-	getPostContent(req.params.slug, req.params.postTitle, function(post) {
-		res.render('detail', {
-			post: post
-		});
-	});
-});
-
 /* GET site posts list in JSON format  */
 router.get('/api/:slug', function(req, res) {
 
     if (req.params.slug === 'hn') {
-        getPosts('http://api.ihackernews.com/page', 'hacker-news', function(posts) {
+        getPosts('http://api.ihackernews.com/page', req.params.slug, function(posts) {
             res.json(posts);
             return;
         });
     } else if (req.params.slug === 'theverge') {
-        getPosts('http://theverge.com/', 'the-verge', function(posts) {
+        getPosts('http://theverge.com/', req.params.slug, function(posts) {
             res.json(posts);
             return;
         });
@@ -64,7 +55,16 @@ router.get('/api/:slug', function(req, res) {
         res.jsonp(404, { error: "No posts available. Wrong API Endpoint."});
     }
 
+});
 
+/* GET post */
+router.get('/:slug/:postTitle', function(req, res) {
+    console.log(req.params.slug, req.params.postTitle);
+    getPostContent(req.params.slug, req.params.postTitle, function(post) {
+        res.render('detail', {
+            post: post
+        });
+    });
 });
 
 module.exports = router;
