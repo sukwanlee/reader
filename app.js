955d6d580fef3531ca637f01b1cf5b80006e8431
Sukwan Lee <lee.sukwan@gmail.com>
2014-08-23 18:35:07 -0700

    mongo fix and code refactoring

--- app.js
+++ app.js
@@ -6,9 +6,21 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
+
+var mongoose = require('mongoose');
+
+// mongodb setup
+var uristring = process.env.MONGOLAB_URI || 'mongodb://localhost/reader';
+
+mongoose.connect(uristring, function(err, res) {
+  if (err) {
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log('Succeeded connecting to: ' + uristring);
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +35,6 @@ app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,15 @@
 var express = require('express'),
-        request = require('request'),
-        NodeCache = require('node-cache'),
-        mongoose = require('mongoose'),
-        sites = require('../resources/sites'),
-        Post = require('../models/post');
+    sites = require('../resources/sites'),
+    listHelper = require('../lib/lists');
 
 var router = express.Router();
-
-mongoose.connect('mongodb://localhost/reader');
-
-/* Initialize Caches */
-
-// postCache settings, in seconds
-var POST_TTL = 2*24*60*60,
-        POST_CHECK_PERIOD = 60*60;
-
-var postCache = new NodeCache({
-    stdTTL: POST_TTL,
-    checkperiod: POST_CHECK_PERIOD
-});
-
-// listCache settings, in seconds
-var LIST_TTL = 5*60,
-        LIST_CHECK_PERIOD = 30;
-
-var listCache = new NodeCache({
-    stdTTL: LIST_TTL,
-    checkperiod: LIST_CHECK_PERIOD,
-});
-
-
-
 
 
 /* GET home page */
 router.get('/', function(req, res) {
+	listCache.get('hacker-news', function(err, value) {
+		console.log(value);
+	});
     res.render('home', {
         sites: sites
     });
@@ -43,11 +18,10 @@ router.get('/', function(req, res) {
 
 /* GET site posts list */
 router.get('/:slug', function(req, res) {
-
     var posts;
 
     if (req.params.slug === 'hn') {
-        getPostsHackerNews(function(posts) {
+        listHelper('http://api.ihackernews.com/page', 'hacker-news', function(posts) {
             res.render('list', {
                 posts: posts,
                 title: 'Hacker News',
@@ -55,8 +29,12 @@ router.get('/:slug', function(req, res) {
             return;
         });
     } else if (req.params.slug === 'theverge') {
-        posts = getPostsTheVerge();
-        title = 'The Verge';
+        listHelper('http://theverge.com/', 'the-verge', function(posts) {
+        	res.render('list', {
+        		posts: posts,
+        		title: 'The Verge'
+        	});
+        });
     } else {
         res.status(404).send('That page is not yet supported :(');
     }
@@ -64,111 +42,7 @@ router.get('/:slug', function(req, res) {
 });
 
 
-var getPostsHackerNews = function(callback) {
-    var hnUrl = 'http://api.ihackernews.com/page';
 
-    listCache.get('hacker-news', function(err, posts) {
-        if (!err && posts.length > 0) {
-            // if cache is not empty, execute callback using the cached posts
-            callback(posts);
-        } else if (!err) {
-            // if cache is empty, request for a new list of posts, store them in db if doesn't already exist
-            request({
-                url: hnUrl,
-                json: true
-            }, function(err, res, body) {
-                if (err) {
-                    res.status(500).send('Internal Error!');
-                    return {};
-                }
-                var posts = res.body.items,
-                            i;
-
-                for (i = 0; i < posts.length; i++) {
-                    Post.findOne({
-                        'title': posts[i].title,
-                        'url': posts[i].url
-                    }, function(err, result) {
-                        if (err) {
-                            console.log(err);
-                        }
-                        if (result) {
-                            new Post({
-                                title: result.title,
-                                url: result.url,
-                                website: 'hacker-news',
-                                slug: result.title.replace(/ /g, '-').trim().toLowerCase()
-                            }).save(function(err) {
-                                if(err) {
-                                    console.log(err);
-                                }
-                            });
-                        }
-                    });
-                }
-                callback(posts);
-            });
-        } else {
-            res.status(500).send('Something went wrong while getting the cache value for hacker news');
-        }
-    });
-
-		var hnUrl = 'http://api.ihackernews.com/page';
-
-		listCache.get('hacker-news', function(err, posts) {
-			if (!err && posts.length > 0) {
-				// if cache is not empty, execute callback using the cached posts
-				callback(posts);
-			} else if (!err) {
-				// if cache is empty, request for a new list of posts, store them in db if doesn't already exist
-				request({
-					url: hnUrl,
-					json: true
-				}, function(err, res, body) {
-					if (err) {
-						res.status(500).send('Internal Error!');
-						return {};
-					}
-					var posts = res.body.items,
-							i;
-
-					for (i = 0; i < posts.length; i++) {
-						Post.findOne({
-							'title': posts[i].title,
-							'url': posts[i].url
-						}, function(err, result) {
-							if (err) {
-								console.log(err);
-							} else {
-								new Post({
-									title: result.title,
-									url: result.url,
-									website: 'hacker-news',
-									slug: result.title.replace(/ /g, '-').trim().toLowerCase()
-								}).save(function(err) {
-									if(err) {
-										console.log(err);
-									}
-								});
-							}
-						});
-					}
-					callback(posts);
-				});
-			} else {
-				res.status(500).send('Something went wrong while getting the cache value for hacker news');
-			}
-		});
-};
-
-var getPostsTheVerge = function() {
-
-};
-
-
-
-
-/* GET home page. */
 router.get('/sdf', function(req, res) {
     request({
         url: 'http://readability.com/api/content/v1/parser?url=http://gizmodo.com/the-chainsmokers-kanye-1622738472&token=38d99fa737228f98b6e6ccdf236ed1b6fcb2a184',
